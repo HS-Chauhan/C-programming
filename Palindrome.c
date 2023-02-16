47152cbb4417781c7f5390c6ae36be2f553d3fd8
Harshvardhan Singh Chauhan <95760077+Harshvardhan0042@users.noreply.github.com>
2023-02-16 18:59:23 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, reverse = 0;
+    printf("Enter a digit: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp != 0)
+    {
+        reverse = reverse * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    if (n == reverse)
+    {
+        printf("%d is a palindrome.\n", n);
+    }
+    else
+    {
+        printf("%d is not a palindrome.\n", n);
+    }
+
+    return 0;
+}
